067473d3626d175616484dad7907be1b243d5f20
mrmerlin320 <mrmerlin320@gmail.com>
2025-02-18 07:42:42 +0000

    update logs

--- update.sh
+++ update.sh
@@ -19,7 +19,7 @@ function checkModule(){
   fi
 }
 
-echo "Set ipv6 address on each node"
+
 docker exec kind-control-plane ip -6 a a 2001:20::8/64 dev eth0
 docker exec kind-worker ip -6 a a 2001:20::1/64 dev eth0
 docker exec kind-worker2 ip -6 a a 2001:20::2/64 dev eth0
